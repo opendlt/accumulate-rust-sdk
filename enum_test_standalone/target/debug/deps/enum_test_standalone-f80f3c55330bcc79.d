066003c8e69ac6d290ad5d99545871d79e86dae9
Jason-Gregoire <jasonkgregoire@gmail.com>
2025-10-04 10:43:08 -0400

    chore: remove deprecated enum_test_standalone project and artifacts

--- enum_test_standalone/target/debug/deps/enum_test_standalone-f80f3c55330bcc79.d
+++
@@ -1,9 +0,0 @@
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\enum_test_standalone-f80f3c55330bcc79.d: src\lib.rs src\comprehensive_tests.rs src\../../src/generated/enums.rs
-
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\enum_test_standalone-f80f3c55330bcc79.exe: src\lib.rs src\comprehensive_tests.rs src\../../src/generated/enums.rs
-
-src\lib.rs:
-src\comprehensive_tests.rs:
-src\../../src/generated/enums.rs:
-
-# env-dep:CARGO_MANIFEST_DIR=C:\\Accumulate_Stuff\\opendlt-rust-v2v3-sdk\\unified\\enum_test_standalone
--- enum_test_standalone/target/debug/deps/enum_test_standalone.d
+++
@@ -1,5 +0,0 @@
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\enum_test_standalone.d: src\main.rs
-
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\enum_test_standalone.exe: src\main.rs
-
-src\main.rs:
