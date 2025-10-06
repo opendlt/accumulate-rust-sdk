f3fab45b2d04e079d8114afaf386d12f35bd8c55
Jason-Gregoire <jasonkgregoire@gmail.com>
2025-10-06 09:07:39 -0400

    repo clean up

--- enum_test_standalone/target/debug/deps/enum_test_standalone-64af16f731c7c809.d
+++
@@ -1,5 +0,0 @@
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\enum_test_standalone-64af16f731c7c809.d: src\main.rs
-
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\enum_test_standalone-64af16f731c7c809.exe: src\main.rs
-
-src\main.rs:
--- enum_test_standalone/target/debug/deps/unicode_ident-189ca15178b593cc.d
+++
@@ -1,8 +0,0 @@
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\unicode_ident-189ca15178b593cc.d: C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
-
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\libunicode_ident-189ca15178b593cc.rlib: C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
-
-C:\Accumulate_Stuff\opendlt-rust-v2v3-sdk\unified\enum_test_standalone\target\debug\deps\libunicode_ident-189ca15178b593cc.rmeta: C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs
-
-C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\lib.rs:
-C:\Users\jason\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.19\src\tables.rs:
